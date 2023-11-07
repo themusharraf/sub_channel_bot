b804397a659cafc869d1c66b567e2d6fe854d026
allnc <meibrohimov@gmail.com>
2023-11-08 00:24:12 +0500

    Fixed main.py 🟢

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 from markups import keyboard, checkSubMenu
 from root import settings
+from aiogram.types import CallbackQuery
 
 logging.basicConfig(level=logging.INFO)
 
@@ -48,14 +49,15 @@ async def main():
     await dp.start_polling(bot)
 
 
-@dp.callback_query(lambda c: c.data =="subchanneldone")
-async def subchanneldone(call: types.CallbackQuery,message: types.Message):
-    await bot.delete_message(message.from_user.id, message.message_id)
+@dp.callback_query(lambda c: c.data == "subchanneldone")
+async def subchanneldone(call: CallbackQuery):
+    await bot.delete_message(call.message.chat.id, call.message.message_id)
+
     if check_sup_channel(
-            chat_member=await bot.get_chat_member(chat_id=settings.bots.channel_id, user_id=message.from_user.id)):
-        await bot.send_message(message.from_user.id, "salom", reply_markup=keyboard)
+            chat_member=await bot.get_chat_member(chat_id=settings.bots.channel_id, user_id=call.from_user.id)):
+        await bot.send_message(call.from_user.id, "salom", reply_markup=keyboard)
     else:
-        await bot.send_message(message.from_user.id, not_sub_message, reply_markup=checkSubMenu)
+        await bot.send_message(call.from_user.id, not_sub_message, reply_markup=checkSubMenu)
 
 if __name__ == "__main__":
     asyncio.run(main())
